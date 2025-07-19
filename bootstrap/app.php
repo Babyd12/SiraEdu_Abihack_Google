6e72363ecf024900c2874cd00a03d47dec06522d
leyeleye22 <146863959+leyeleye22@users.noreply.github.com>
2025-07-19 08:51:53 +0000

    <fixed> initial commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\isSuperAdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,9 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
+        // $middleware->api(prepend: [
+        //     isSuperAdminMiddleware::class,
+        // ]);
+        $middleware->alias([
+            'superAdmin' => isSuperAdminMiddleware::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Http\Controllers\API\ApiController;
+use App\Http\Controllers\API\RoleController;
+use App\Http\Middleware\isSuperAdminMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post('register', [ApiController::class, 'register']);
+Route::post('login', [ApiController::class, 'login']);
+//Route protected 
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('logout', [ApiController::class, 'logout']);
+    Route::get('profile', [ApiController::class, 'profile']);
+    Route::put('update-profile/{user}', [ApiController::class, 'updateProfile']);
+    Route::get('refreshToken', [ApiController::class, 'refreshToken']);
+});
+
+Route::apiResource('role', RoleController::class)->middleware('superAdmin');
+
+//if user is not logged in
+Route::get('login-auth', function (){
+return response()->json([
+    'error' => 'Unauthenticated',
+], 404);
+})->name('login');
